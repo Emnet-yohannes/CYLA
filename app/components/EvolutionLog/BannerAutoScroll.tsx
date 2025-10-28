cd40ccd725a2f9f18a3f11e96271bfdea637de00
emnet yohannes <emnet.yohan@gmail.com>
2025-10-28 14:20:22 +0300

    text changes

--- app/components/EvolutionLog/BannerAutoScroll.tsx
+++ app/components/EvolutionLog/BannerAutoScroll.tsx
@@ -24,12 +24,12 @@ const BannerAutoScroll = () => {
               <p className="text-lg sm:text-2xl">EXPLORER</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The trader</p>
-              <p className="text-lg sm:text-2xl">extraordinaire</p>
+              <p className="text-xs">ECOSYSTEM</p>
+              <p className="text-lg sm:text-2xl">OBSERVANT</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The prediction</p>
-              <p className="text-lg sm:text-2xl">powerhouse</p>
+              <p className="text-xs">THE LEARNING</p>
+              <p className="text-lg sm:text-2xl">ENGINE</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
               <p className="text-xs">ON-CHAIN</p>
@@ -40,18 +40,18 @@ const BannerAutoScroll = () => {
               <p className="text-lg sm:text-2xl">AWARENESS</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The price</p>
-              <p className="text-lg sm:text-2xl">oracle</p>
+              <p className="text-xs">REAL-TIME</p>
+              <p className="text-lg sm:text-2xl">INTELLIGENCE</p>
             </div>
           </div>
           <div className="animate-ticker flex gap-4">
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The casual</p>
-              <p className="text-lg sm:text-2xl">bettor</p>
+              <p className="text-xs">ADAPTIVE</p>
+              <p className="text-lg sm:text-2xl">LEARNING</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The crypto</p>
-              <p className="text-lg sm:text-2xl">enthusiast</p>
+              <p className="text-xs">ON-CHAIN</p>
+              <p className="text-lg sm:text-2xl">EXPLORER</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
               <p className="text-xs">ECOSYSTEM</p>
@@ -62,12 +62,12 @@ const BannerAutoScroll = () => {
               <p className="text-lg sm:text-2xl">ENGINE</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The social</p>
-              <p className="text-lg sm:text-2xl">expert</p>
+              <p className="text-xs">ON-CHAIN</p>
+              <p className="text-lg sm:text-2xl">SIGNALS</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
-              <p className="text-xs">The analytics</p>
-              <p className="text-lg sm:text-2xl">guru</p>
+              <p className="text-xs">MARKET</p>
+              <p className="text-lg sm:text-2xl">AWARENESS</p>
             </div>
             <div className="space-y-1 flex-shrink-0 min-w-[250px] flex-col flex items-center justify-center border-primary text-primary border backdrop-blur-sm px-6 py-2 font-nb-architekt">
               <p className="text-xs">REAL-TIME</p>
